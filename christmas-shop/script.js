efc68055549483c267ed0d20f07aefa8d0b816c4
Aleksei Zhuchkov <kelzzzer@gmail.com>
2024-11-23 22:26:52 +0300

    feat: complete animation for burger

--- christmas-shop/script.js
+++ christmas-shop/script.js
@@ -1,17 +1,26 @@
-"use strict"
+"use strict";
 
-let arr = document.querySelectorAll('.button-black')
+let arr = document.querySelectorAll(".button-black");
 
-arr.forEach(el => {
-  el.addEventListener('click', ()=> {
-    let path = window.location.href
-    if(path.includes('localhost')){
-      let position = path.lastIndexOf('/')
-      path = path.slice(0, position)
-      window.location.href = path + '/gifts/'
+arr.forEach((el) => {
+  el.addEventListener("click", () => {
+    let path = window.location.href;
+    if (path.includes("localhost")) {
+      let position = path.lastIndexOf("/");
+      path = path.slice(0, position);
+      window.location.href = path + "/gifts/";
     } else {
-      let pathProduction = "https://rolling-scopes-school.github.io/kelzerock-JSFE2024Q4/christmas-shop/gifts/";
+      let pathProduction =
+        "https://rolling-scopes-school.github.io/kelzerock-JSFE2024Q4/christmas-shop/gifts/";
       window.location.href = pathProduction;
     }
-  })
-})
+  });
+});
+
+//burger
+const burgerLine = document.querySelectorAll(".header-burger-button-line");
+const burger = document.querySelector(".burger");
+
+burger.addEventListener("click", () => {
+  burgerLine.forEach(el => el.classList.toggle("active"));
+});
