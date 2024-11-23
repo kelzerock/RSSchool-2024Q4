cc239187493eedc5233394772737b573492530e8
Aleksei Zhuchkov <kelzzzer@gmail.com>
2024-11-24 01:15:36 +0300

    feat: burger on main page

--- christmas-shop/script.js
+++ christmas-shop/script.js
@@ -18,9 +18,47 @@ arr.forEach((el) => {
 });
 
 //burger
-const burgerLine = document.querySelectorAll(".header-burger-button-line");
+const bodyHTML = document.body;
+const burgerLines = document.querySelectorAll(".header-burger-button-line");
 const burger = document.querySelector(".burger");
+const burgerMenu = document.querySelector(".burger-menu ");
+const burgerMenuItems = document.querySelectorAll(".burger-menu-item")
 
-burger.addEventListener("click", () => {
-  burgerLine.forEach(el => el.classList.toggle("active"));
-});
+const toggleActiveStatus = () => {
+  burgerLines.forEach(el => el.classList.toggle("active"));
+  burgerMenu.classList.toggle("active");
+  bodyHTML.classList.toggle("lock");
+}
+
+burger.onclick = () => {
+  toggleActiveStatus();
+}
+
+burgerMenu.onclick = (event) => {
+  const liTag = event.target.closest('li');
+  if(Array.from(burgerMenuItems).includes(liTag)){
+    toggleActiveStatus();
+  }
+}
+
+// burgerMenuItems.forEach((el) => {
+//   el.onclick = () => {
+//     toggleActiveStatus();
+//   }
+// })
+
+// burgerMenu.addEventListener("click", () => {
+//   console.log(event.currentTarget)
+//   console.log(event.target)
+//   toggleActiveStatus();
+// })
+
+
+window.addEventListener("resize", (event) => {
+  const widthWindow = window.innerWidth;
+  if (widthWindow > 768) {
+    if (burgerMenu.classList.contains("active")) {
+      toggleActiveStatus()
+    }
+  }
+})
