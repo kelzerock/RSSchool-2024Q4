3394f29ae517c4d5582f60d681ee4a5d936dfb79
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-01-09 22:37:55 +0300

    init: start project simon-say + install prettier and eslint to the project

---
+++ simon-says/eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+
+// import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {...globals.browser, ...globals.node},
+    }
+  },
+  {plugins: {
+    prettier: eslintPluginPrettier
+  }},
+  {
+    ignores: ["node_modules", "dist"]
+  },
+  {
+    files: ["**/*.js"],
+    rules: {
+      "prefer-const": "error"
+    }
+  }
+];
