806c0f42d219c31f1077bf586b182c0cc72a1870
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-01-11 19:30:46 +0300

    feat: set webpackconfig

--- simon-says/eslint.config.mjs
+++ simon-says/eslint.config.mjs
@@ -1,27 +1,31 @@
 import js from "@eslint/js";
 import globals from "globals";
 import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 // import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
+  eslintConfigPrettier,
   {
     languageOptions: {
-      globals: {...globals.browser, ...globals.node},
-    }
+      globals: { ...globals.browser, ...globals.node },
+    },
   },
-  {plugins: {
-    prettier: eslintPluginPrettier
-  }},
   {
-    ignores: ["node_modules", "dist"]
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+  },
+  {
+    ignores: ["node_modules", "dist"],
   },
   {
     files: ["**/*.js"],
     rules: {
-      "prefer-const": "error"
-    }
-  }
+      "prefer-const": "error",
+    },
+  },
 ];
--- simon-says/webpack.config.js
+++ simon-says/webpack.config.js
@@ -1,10 +1,37 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+module.exports = (env) => {
+  const typeBuild = env.production ? "production" : "development";
+
+  return {
+    mode: typeBuild,
+    entry: "./src/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "index.html"),
+      }),
+    ],
+    devtool: "inline-source-map",
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      compress: true,
+      port: 9000,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+      ],
+    },
+  };
 };
