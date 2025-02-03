9c63d4e0410b329d0c5cebd50ca058c41ec314b1
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-02-03 13:10:45 +0300

    feat: complete task

--- nonograms/src/js/footer.js
+++ nonograms/src/js/footer.js
@@ -1,10 +1,23 @@
 import { Component } from "./node";
 import "../assets/style/footer.scss";
 
-const footer = new Component({
-  tag: "footer",
-  className: "footer",
-  text: "Footer",
+const link = new Component({
+  tag: "a",
+  className: "footer-link",
+  text: "Created by Zhuchkov Aleksei, 2025",
+  attributes: { href: "https://github.com/zhuchkov" },
 });
+
+link.getNode().setAttribute("target", "_blank");
+link.getNode().setAttribute("href", "https://github.com/zhuchkov");
+
+const footer = new Component(
+  {
+    tag: "footer",
+    className: "footer",
+    text: "",
+  },
+  link
+);
 
 export { footer };
