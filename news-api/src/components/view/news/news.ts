20a9da59fece7d6b5b13a0a3fa616ac3c57dfb6d
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-02-20 23:16:00 +0300

    feat: complete migration news.js to TypeScript

---
+++ news-api/src/components/view/news/news.ts
@@ -0,0 +1,42 @@
+import { NewsAPI } from '../../../types/index';
+import './news.css';
+
+class News {
+    draw(data: NewsAPI[]) {
+        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+
+        const fragment = document.createDocumentFragment();
+        const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
+
+        news.forEach((item, idx) => {
+            const newsClone = newsItemTemp.content.cloneNode(true) as DocumentFragment;
+
+            if (idx % 2) (newsClone.querySelector('.news__item') as HTMLDivElement).classList.add('alt');
+
+            (newsClone.querySelector('.news__meta-photo') as HTMLDivElement).style.backgroundImage = `url(${
+                item.urlToImage || 'img/news_placeholder.jpg'
+            })`;
+            (newsClone.querySelector('.news__meta-author') as HTMLLIElement).textContent =
+                item.author || item.source.name;
+            (newsClone.querySelector('.news__meta-date') as HTMLLIElement).textContent = item.publishedAt
+                .slice(0, 10)
+                .split('-')
+                .reverse()
+                .join('-');
+
+            (newsClone.querySelector('.news__description-title') as HTMLHeadingElement).textContent = item.title;
+            (newsClone.querySelector('.news__description-source') as HTMLHeadingElement).textContent = item.source.name;
+            (newsClone.querySelector('.news__description-content') as HTMLParagraphElement).textContent =
+                item.description;
+            (newsClone.querySelector('.news__read-more a') as HTMLLinkElement).setAttribute('href', item.url);
+
+            fragment.append(newsClone);
+        });
+
+        const newsContainer = document.querySelector('.news') as HTMLDivElement;
+        newsContainer.innerHTML = '';
+        newsContainer.appendChild(fragment);
+    }
+}
+
+export default News;
