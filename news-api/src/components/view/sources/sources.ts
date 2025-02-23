33bf86176baa40976340c80644486a03faacd769
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-02-23 14:44:13 +0300

    refactor: complete sources without queryselectors

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -1,24 +1,33 @@
 import { SourceApi } from '../../../types/index';
+import { handleError } from '../../../utils/utils';
 import './sources.css';
+
+enum Names {
+    sources = 'sources',
+    sourceItemName = 'source__item-name',
+    sourceItem = 'source__item',
+    dataAttr = 'data-source-id',
+}
 
 class Sources {
     draw(data: SourceApi[]) {
         const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
-        const sourceDiv = document.querySelector('.sources') as HTMLDivElement; // ).append(fragment);
+        const sourceDiv = document.querySelector(`.${Names.sources}`);
 
-        if (sourceItemTemp && sourceItemTemp) {
+        if (sourceDiv) {
             data.forEach((item) => {
-                const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
-
-                (sourceClone.querySelector('.source__item-name') as HTMLSpanElement).textContent = item.name;
-                (sourceClone.querySelector('.source__item') as HTMLDivElement).setAttribute('data-source-id', item.id);
-
-                fragment.append(sourceClone);
+                const sourceItemName = document.createElement('div');
+                sourceItemName.classList.add(Names.sourceItemName);
+                const sourceItem = document.createElement('span');
+                sourceItem.classList.add(Names.sourceItem);
+                sourceItemName.textContent = item.name;
+                sourceItem.setAttribute(Names.dataAttr, item.id);
+                sourceItem.append(sourceItemName);
+                fragment.append(sourceItem);
             });
             sourceDiv.append(fragment);
         } else {
-            console.error("Attention! Div .source or #sourceItemTemp doesn't exist in HtmlDOM");
+            handleError([Names.sources]);
         }
     }
 }
--- news-api/src/utils/utils.ts
+++ news-api/src/utils/utils.ts
@@ -1,3 +1,5 @@
 export const handleError = (args: string[]): void => {
-    console.error(`Attentions! This element${args.length > 1 ? 's' : ''} ${args.join(', ')} doesn't exist in HtmlDOM.`);
+    console.error(
+        `Attentions! This element${args.length > 1 ? 's' : ''} ${args.map((el) => `"${el}"`).join(', ')} doesn't exist in HtmlDOM.`
+    );
 };
