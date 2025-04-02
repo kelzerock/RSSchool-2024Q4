1c4179c84247c18627568860314a17cc74d7f6e8
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-04-02 23:44:48 +0300

    feat: test hook

--- async-race/webpack-config/bundle-plugins.ts
+++ async-race/webpack-config/bundle-plugins.ts
@@ -4,6 +4,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import { Configuration } from "webpack"
 
 export const bundlePlugins = (option: optionsWebpack): Configuration["plugins"] => {
+  const isDev = option.mode === "dev"
   const plugins = [
     new HtmlWebpackPlugin({
       template: path.resolve(option.path.template, "index.html"),
